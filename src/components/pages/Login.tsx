8b7439b1d093d710b92790281c5c74cc1f1e8f54
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-17 08:51:58 +0900

    Login.tsxの中でUserIDを取得する部分を実装 useStateを利用してuserID をゲットしてセットするというよくある実装。 ポイントは inputが変更されたときにイベントを受け取るのの型指定 ChangeEvent<HTMLInputElement> というのを使う。これは暗記するもの。

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -8,10 +8,18 @@ import {
   Stack
 } from "@chakra-ui/react";
 
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useState, VFC } from "react";
 import { PrimaryButton } from "../button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  // ログインインプット文字列のところから値を取得するための useState
+  // TypeScriptを使っていてもuseStateには初期値を（）内で渡すと型推定をしてくれるので簡単な場合には指定する必要はない
+  const [userID, setUserId] = useState("");
+  // テキストボックスの値が変化した場合のトレース
+  // Inputから得られるイベントの型指定は覚えるしか無い！
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       {/* 背景を白にして幅を狭め、角丸にして影をつけた */}
@@ -23,7 +31,13 @@ export const Login: VFC = memo(() => {
         <Divider my={5} />
         {/* Stackは特におすすめでこのTagで囲ったものを等間隔に配置するという便利な代物 */}
         <Stack spacing={5} py={4} px={10}>
-          <Input placeholder="ユーザ名" />
+          <Input
+            placeholder="ユーザ名"
+            // Inputに入力された値を表示する。useStateで管理している値
+            value={userID}
+            // Inputに入力された値が変化したとき useState で値を設定する
+            onChange={onChangeUserId}
+          />
           <PrimaryButton> ログイン </PrimaryButton>
         </Stack>
       </Box>
