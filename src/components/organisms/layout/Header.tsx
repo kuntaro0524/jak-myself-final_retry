eaceb2da8eaad248ded3dcb93f202cac44c07a0b
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-16 11:57:57 +0900

    Drawerをハンバーガーメニューを押したらだす という実装を行った。 プレビューが死んでいるような気がするがとりあえずコミットする（セーブ）

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,55 +1,85 @@
-import { Flex, Heading, IconButton } from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  IconButton,
+  useDisclosure
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Link } from "react-router-dom";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
+  // Drawerを描くとか開くとか閉じるとかいう chakra-UIが提供している hooks を利用する
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   // Flex boxみたいなものが簡単に実現する
   // naviタグにしてやる
   // bg: 色を指定
   // color: 文字の色
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="space-between"
-      // paddingについても動的な設定が可能→ breakpoint md のときに 5px に切り替えるという設定
-      padding={{ base: 3, md: 5 }}
-    >
-      {/* h1タグのように使える */}
-      {/* base: ベースのフォントサイズなども指定できる
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        // paddingについても動的な設定が可能→ breakpoint md のときに 5px に切り替えるという設定
+        padding={{ base: 3, md: 5 }}
+      >
+        {/* h1タグのように使える */}
+        {/* base: ベースのフォントサイズなども指定できる
       break point(画面サイズのしきい値)ごとに文字のサイズを設定することができる
       この例だと 画面サイズが md 以上のときに　フォントサイズを lg にする 
       Flexで囲ってあげてクリックできるようにするために a タグとする
       右側マージン 8px
       カーソルのポインタを変更するのも設定*/}
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          ユーザ管理アプリ
-        </Heading>
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザ管理アプリ
+          </Heading>
+        </Flex>
+        {/* ページへのリンクを貼る */}
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Link> ユーザー一覧 </Link>
+          <Link> 設定 </Link>
+        </Flex>
+        {/* 画面が小さいときにハンバーガーメニューを表示したい */}
+        <IconButton
+          aria-label="Menu button"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          // base: 携帯のときは、みたいなこと（画面サイズ）、md以上で表示なしという設定
+          display={{ base: "block", md: "none" }}
+          // クリックしたときに Drawer をオープするかどうかというカスタムフックを利用する
+          onClick={onOpen}
+        />
       </Flex>
-      {/* ページへのリンクを貼る */}
-      <Flex
-        align="center"
-        fontSize="sm"
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
-      >
-        <Link> ユーザー一覧 </Link>
-        <Link> 設定 </Link>
-      </Flex>
-      {/* 画面が小さいときにハンバーガーメニューを表示したい */}
-      <IconButton
-        aria-label="Menu button"
-        icon={<HamburgerIcon />}
-        size="sm"
-        variant="unstyled"
-        // base: 携帯のときは、みたいなこと（画面サイズ）、md以上で表示なしという設定
-        display={{ base: "block", md: "none" }}
-      />
-    </Flex>
+      {/* 左から表示を出す: placement="left" 
+      sizeも指定できる　*/}
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody>
+              <Button> Top </Button>
+              <Button> ユーザ一覧 </Button>
+              <Button> 設定画面 </Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 });
 
