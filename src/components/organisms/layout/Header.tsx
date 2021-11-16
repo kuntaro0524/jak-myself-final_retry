321c08f99713cba8e893f6288b9bc9dc58c37d7c
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-16 10:31:30 +0900

    Headerの部分を整形している Headerの部分に<Flex>とか<Link> を入れて、ブレイクポイントも意識した実装をした。

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { Link } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
   // Flex boxみたいなものが簡単に実現する
@@ -19,10 +20,25 @@ export const Header: VFC = memo(() => {
       {/* h1タグのように使える */}
       {/* base: ベースのフォントサイズなども指定できる
       break point(画面サイズのしきい値)ごとに文字のサイズを設定することができる
-      この例だと 画面サイズが md 以上のときに　フォントサイズを lg にする */}
-      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-        ユーザ管理アプリ
-      </Heading>
+      この例だと 画面サイズが md 以上のときに　フォントサイズを lg にする 
+      Flexで囲ってあげてクリックできるようにするために a タグとする
+      右側マージン 8px
+      カーソルのポインタを変更するのも設定*/}
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          ユーザ管理アプリ
+        </Heading>
+      </Flex>
+      {/* ページへのリンクを貼る */}
+      <Flex
+        align="center"
+        fontSize="sm"
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+      >
+        <Link> ユーザー一覧 </Link>
+        <Link> 設定 </Link>
+      </Flex>
     </Flex>
   );
 });
