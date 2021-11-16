071975a8ab79f9e6cfcb06b2cd4f981b8d25db17
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-16 21:53:42 +0900

    Login画面を実装した Flex-Box を起点に ヘッダ部分で <Heading> ディバイダ部分で <Divider> 等間隔に並べるツールとして <Stack> 入力文字列ように <Input> ボタンで<Button> をそれぞれ chakraUIから利用した。 特にStackが便利なので覚えたほうが良さげ

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,32 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p> ログインページです </p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      {/* 背景を白にして幅を狭め、角丸にして影をつけた */}
+      <Box bg="white" w="sm" p={4} borderRardius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          Unko
+        </Heading>
+        {/* デバイダ→横線の区切り */}
+        <Divider my={5} />
+        {/* Stackは特におすすめでこのTagで囲ったものを等間隔に配置するという便利な代物 */}
+        <Stack spacing={5} py={4} px={10}>
+          <Input placeholder="ユーザ名" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
