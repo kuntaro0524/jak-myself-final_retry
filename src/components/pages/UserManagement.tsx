c96259b00fb3c9f1d216dfa612dad849abbc8486
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-24 14:09:49 +0900

    カードになるコンポーネントを作成 １）じゃけぇ先生の方法としては、最初に見えるものを何らか作っておいて
    ２）それをコンポーネントに切り出して
    
    ３）変数の渡し方を変更していく
    
    という感じかなと思う。実際にコードを書いていてある程度スタイルを決定して表示してみることって大切だと思う。

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,21 +1,41 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem, Box, Stack, Image, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    // spacingをしたいときはここに噛ませてあげる
-    <Wrap spacing="30px">
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <div
-            style={{
-              width: "100px",
-              height: "100px",
-              backgroundColor: "teal"
-            }}
+    // paddingの設定を画面の大きさで分ける
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          textAlign="center"
+          // Boxにhoverしたときの挙動を決める
+          // カーソルがポインタになり、透明度を0.8にする
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          {/* Stackで囲んでおくとスペーシングとかが楽ちんになる */}
+          <Stack> Kunio Hirata </Stack>
+          <Image
+            boxSize="160px"
+            borderRadius="full"
+            src="https://source.unsplash.com/random"
+            alt="プロフィール画像"
+            m="auto"
           />
-        </WrapItem>
-      ))}
+          <Text fontSize="lg" fontWeight="bold">
+            ひらたくにお
+          </Text>
+          <Text fontSize="sm" color="gray">
+            {" "}
+            おにくさんと呼んでくれ{" "}
+          </Text>
+        </Box>
+      </WrapItem>
     </Wrap>
   );
 });
