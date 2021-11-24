077de33584d3c98508a9ff70e9689ea26d1506d4
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-24 14:26:34 +0900

    さっき調整していたカードをファイル分割 コンポーネントとしてファイルを分割して使い回しが効くようにした。 Propsの型設定なども必見。 拡張子が .tsx でないと Props の type 指定などもできないんすね。

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,44 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { memo, ReactNode, VFC } from "react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  // propsで渡されない場合には disabled と loading は　デフォルト false
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      textAlign="center"
+      // Boxにhoverしたときの挙動を決める
+      // カーソルがポインタになり、透明度を0.8にする
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      {/* Stackで囲んでおくとスペーシングとかが楽ちんになる */}
+      <Stack>
+        <Image
+          boxSize="160px"
+          borderRadius="full"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,40 +1,18 @@
 import { Wrap, WrapItem, Box, Stack, Image, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManagement: VFC = memo(() => {
   return (
     // paddingの設定を画面の大きさで分ける
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          textAlign="center"
-          // Boxにhoverしたときの挙動を決める
-          // カーソルがポインタになり、透明度を0.8にする
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          {/* Stackで囲んでおくとスペーシングとかが楽ちんになる */}
-          <Stack> Kunio Hirata </Stack>
-          <Image
-            boxSize="160px"
-            borderRadius="full"
-            src="https://source.unsplash.com/random"
-            alt="プロフィール画像"
-            m="auto"
-          />
-          <Text fontSize="lg" fontWeight="bold">
-            ひらたくにお
-          </Text>
-          <Text fontSize="sm" color="gray">
-            {" "}
-            おにくさんと呼んでくれ{" "}
-          </Text>
-        </Box>
+        <UserCard
+          userName="くんたろーさん"
+          fullName="平田邦生"
+          imageUrl="https://source.unsplash.com/random"
+        />
       </WrapItem>
     </Wrap>
   );
