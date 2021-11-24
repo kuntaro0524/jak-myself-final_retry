2c6768dde4da1db15c306f2a171df7d133f904fe
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-24 14:54:57 +0900

    user情報を制御する部分を切り出したカスタムフック２ 骨格からはじめて実装を進めた。 とりあえずこれで機能を確認してみるというところまで書いたので、変更点がわかりやすようにここで一回切っておく。

--- src/hooks/useAllUsers.ts
+++ src/hooks/useAllUsers.ts
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import axios from "axios";
+import { useCallback, useState } from "react";
+
+import { User } from "../components/types/api/user";
+import { showMessage } from "./useMessage";
 
 export const useAllUsers = () => {
-  const [loading, setLoading] = useState();
-  const [users, setUsers] = useState();
+  // 型推論で勝手に boolean　になるところは放置しても良い（型指定）
+  // なんかcodesandboxで文句言われたので boolean 足した
+  const [loading, setLoading] = useState<boolean>();
 
-  const getUsers = () => {};
+  // ユーザの配列として users を定義するということを型指定で明瞭にしておく
+  const [users, setUsers] = useState<Array<User>>();
+
+  const getUsers = useCallback(() => {
+    setLoading(true);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch(() => {
+        // 自作メッセージ表示用のやつ
+        showMessage({ title: "ユーザ取得に失敗しました", status: "error" });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return { getUsers, loading, users };
 };
