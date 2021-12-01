39348a3d8fed9b16ced3ea3c4578f38b8f98ba7a
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-12-01 16:36:41 +0900

    userの情報を保持するためのcontextを設定 LoginUserProvider.tsx にてユーザ情報をどこでも利用できるようにグローバル変数化する手続き（Provider)を行っている。 Providerの中でuseStateとしている変数と設定関数はtype文の中で型指定している。 型指定したもので LoginUserContext をcreateContext していて、 LoginUserContext.Provider で利用している。これにより内部で useStateで定義した変数と設定関数が　Provider　を利用したときに参照できるようになる模様。

---
+++ src/components/providers/LoginUserProvider.tsx
@@ -0,0 +1,34 @@
+// ログインしたユーザの情報を保持する
+import React, {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState
+} from "react";
+
+import { User } from "../types/api/user";
+
+// 保持する型と設定関数
+type LoginUserContextType = {
+  loginUser: User | null;
+  // useState などの更新関数の型は以下のようになるらしい→おぼえげー
+  setLoginUser: Dispatch<SetStateAction<User | null>>;
+};
+
+// Type scriptの表現方法として {} を as で受けて型を指定する
+const LoginUserContext = createContext<LoginUserContextType>(
+  {} as LoginUserContextType
+);
+
+// childrenすべてに影響があるよーって話だったっけ
+export const LoginUserProvider = (props: { children: ReactNode }) => {
+  const { children } = props;
+  // 再レンダリングする規模によっては変数と関数は別にしたほうが良い場合もある
+  const [loginUser, setLoginUser] = useState<User | null>(null);
+  return (
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      {children}
+    </LoginUserContext.Provider>
+  );
+};
